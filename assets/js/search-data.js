396bc92f392a44eaee1755430903d35d2660c7ae
fjsaezm <fjaviersaezm@gmail.com>
2025-08-09 19:36:12 +0000

    Deploying to gh-pages from @ fjsaezm/fjsaezm.github.io@de9dded0663b93fef9d8e716edca747d133d0039 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,7 +451,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-invited-talk-at-seminar-on-artificial-intelligence-in-healthcare",
+          title: 'Invited Talk at Seminar on Artificial Intelligence in Healthcare',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_lyon/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
